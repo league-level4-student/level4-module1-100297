fc30e29384ac7c18ecf004ada1a749857461f030
100297 <league@WTS22.attlocal.net>
2018-09-02 12:56:49 -0700

    overcommit

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -2,34 +2,36 @@ package _01_Custom_ArrayList;
 
 @SuppressWarnings("unchecked")
 
-public class ArrayList <T>{
-	
+public class ArrayList<T> {
+	T[] central;
+
 	public ArrayList() {
+		central = (T[]) new Object[0];
 	}
-	
+
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+
+		return central[loc];
 	}
-	
+
 	public void add(T val) {
-		
+		T[] central1 = (T[]) new Object[central.length + 1];
 	}
-	
+
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+
 	}
-	
+
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+
 	}
-	
+
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+
 	}
-	
+
 	public boolean contains(T val) {
-		
+
 		return false;
 	}
 }
